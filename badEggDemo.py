bae0c179f9ea6065e54a4f29a034abd2a5d5b4a4
spagjoe <44183947+spagjoe@users.noreply.github.com>
2018-10-17 09:02:54 -0400

    Add files via upload

---
+++ badEggDemo.py
@@ -0,0 +1,35 @@
+import tweepy, random
+
+class MyStreamListener(tweepy.StreamListener):
+	def on_status(self, status):
+		username = status.user.screen_name 
+		status_id = status.id
+		replyTweet(username, status_id)
+
+
+	def on_error(self, status):
+		if status_code == 420:
+			return False
+
+def replyTweet(username, status_id):
+	rdm = random.randint(0, 2)
+	if rdm == 0:
+		api.update_status('@{0} You\'re a bad egg.'.format(username), in_reply_to_status_id=status_id)
+	else:
+		api.update_status('@{0} You\'re a good egg.'.format(username), in_reply_to_status_id=status_id)
+
+#keys:
+consumer_key = "xxx"
+consumer_secret = "xxx"
+access_token = "xxx"
+access_token_secret = "xxx"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+myStreamListener = MyStreamListener(api)
+myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
+
+myStream.filter(track = ["@BadEggBot"])
